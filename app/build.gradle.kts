d567c5b1d447ec56d40a0988bcc071c71998697c
Lê Huy Hoàng <156869304+hoangscor@users.noreply.github.com>
2026-06-29 19:33:00 +0700

    Add ViewModel Compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose) // dùng viewModel() trong Compose
 
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
